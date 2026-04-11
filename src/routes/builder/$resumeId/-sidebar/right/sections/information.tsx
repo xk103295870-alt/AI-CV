1a5a8cfa99cd30139b0512829f1c57fbd173e2f0
xiaoke321 <xk103295870@gmail.com>
2026-04-11 19:53:09 +0800

    feat: 移除编辑简历右侧信息面板中的底部链接

--- src/routes/builder/$resumeId/-sidebar/right/sections/information.tsx
+++ src/routes/builder/$resumeId/-sidebar/right/sections/information.tsx
@@ -41,68 +41,6 @@ export function InformationSectionBuilder() {
           }
         />
       </div>
-
-      <div className="flex flex-wrap gap-0.5">
-        <Button
-          size="sm"
-          variant="link"
-          className="text-xs"
-          nativeButton={false}
-          render={
-            <a href="https://docs.rxresu.me" target="_blank" rel="noopener">
-              <Trans>Documentation</Trans>
-            </a>
-          }
-        />
-
-        <Button
-          size="sm"
-          variant="link"
-          className="text-xs"
-          nativeButton={false}
-          render={
-            <a href="https://github.com/amruthpillai/reactive-resume" target="_blank" rel="noopener">
-              <Trans>Source Code</Trans>
-            </a>
-          }
-        />
-
-        <Button
-          size="sm"
-          variant="link"
-          className="text-xs"
-          nativeButton={false}
-          render={
-            <a href="https://github.com/amruthpillai/reactive-resume/issues" target="_blank" rel="noopener">
-              <Trans>Report a Bug</Trans>
-            </a>
-          }
-        />
-
-        <Button
-          size="sm"
-          variant="link"
-          className="text-xs"
-          nativeButton={false}
-          render={
-            <a href="https://crowdin.com/project/reactive-resume" target="_blank" rel="noopener">
-              <Trans>Translations</Trans>
-            </a>
-          }
-        />
-
-        <Button
-          size="sm"
-          variant="link"
-          className="text-xs"
-          nativeButton={false}
-          render={
-            <a href="https://opencollective.com/reactive-resume" target="_blank" rel="noopener">
-              <Trans>Sponsors</Trans>
-            </a>
-          }
-        />
-      </div>
     </SectionBase>
   );
 }
